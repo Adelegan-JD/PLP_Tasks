b5b84f62f834cc7e67b57e6d7d7fa8a55a02436c
Adelegan Deborah <85581813+Adelegan-JD@users.noreply.github.com>
2025-08-17 20:48:42 +0100

    Added first python file via upload

---
+++ python_assignment.py
@@ -0,0 +1,27 @@
+num1 = int(input("Kindly input the first number: "))
+num2 = int(input("Kindly input the second number: "))
+operator = input("What mathematical operation do you want to carry out on these digits?: ")
+ans = num1 + operator + num2
+answer = eval(ans)
+print(ans, " = ", answer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
